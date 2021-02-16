e4c901c81af56bd0bfbf87772e0f5f50d6b66183
oku3san <takaya0303@gmail.com>
2021-02-17 00:27:54 +0900

    Display prerix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,44 @@
 import React from 'react';
+import fetch from 'isomorphic-unfetch';
+import { GetServerSideProps } from 'next';
 
-const Index = () => (
+const Index: any = ({ prefixes }) => (
   <>
-    <div>Hello World</div>
+    <table>
+      <thead>
+        <tr>
+          <th>region</th>
+          <th>service</th>
+          <th>ip_prefix</th>
+        </tr>
+      </thead>
+      <tbody>
+        {/* eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access */}
+        {prefixes.map((prefix, key) => (
+          <tr key={key}>
+            <td>{prefix.region}</td>
+            <td>{prefix.service}</td>
+            <td>{prefix.ip_prefix}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   </>
 );
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const res = await fetch(`https://ip-ranges.amazonaws.com/ip-ranges.json`);
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const ipRanges: any = await res.json();
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const { prefixes } = ipRanges;
+
+  return {
+    props: {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      prefixes,
+    },
+  };
+};
 
 export default Index;
